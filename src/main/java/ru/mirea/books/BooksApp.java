588825ddbdabcc6e49df34bde09193519b0f5fe3
Oleg Sobolev <sobolev_ov@mail.ru>
2021-04-26 21:20:28 +0300

    Аннотация приложения

--- src/main/java/ru/mirea/books/BooksApp.java
+++ src/main/java/ru/mirea/books/BooksApp.java
@@ -1,7 +1,9 @@
 package ru.mirea.books;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 public class BooksApp {
 
     public static void main(String[] args) {
